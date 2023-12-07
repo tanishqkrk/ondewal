617a25b2270afefacd58b3bee3202d3e2b4efd3d
tanishqkrk <tanishqkrk@gmail.com>
2023-12-08 04:50:31 +0530

    PATCHED

--- pages/api/getRemedies.js
+++ pages/api/getRemedies.js
@@ -1,11 +1,23 @@
 import { OpenAI } from "openai";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   console.log(req.body.message);
   // let responseToSend = "default";
   async function fetchResponse(msg) {
     const openai = new OpenAI({
-      apiKey: "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",
+      apiKey: "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",
       // dangerouslyAllowBrowser: true,
     });
     const chatCompletion = await openai.chat.completions.create({
